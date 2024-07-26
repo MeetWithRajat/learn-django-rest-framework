841227c10eba184f7a3a294d6990ab255a1c5ea5
MeetWithRajat <meetwithrajat@gmail.com>
2024-07-26 11:57:06 +0530

    post api code added

--- learning_api/views.py
+++ learning_api/views.py
@@ -1,10 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from learning_api import serializers
 
 
 class HelloAPIView(APIView):
     """Test API View"""
-    def get(self, request, format=None):
+    serializers_class = serializers.HelloSerializer
+
+    @staticmethod
+    def get(request, format=None):
         """Return a list of APIView features"""
         an_apiview = [
             'Uses HTTP methods as function (get, post, patch, put, delete)',
@@ -13,3 +18,14 @@ class HelloAPIView(APIView):
             'Is mapped manually to URL'
         ]
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializers_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hello {name}"
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
